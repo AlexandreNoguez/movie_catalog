87458fc72f46a6c94318ad3e68ff2cde6c6594ea
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2022-05-10 13:22:14 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Form from "./pages/Form";
+import Listing from "./pages/Listing";
 
 export default function App() {
   return (
-      <div>
+      <BrowserRouter>
         <Header />
-      </div>
+          <Routes>
+            <Route path="/" element={<Listing />} />
+            <Route path="/form" >
+              <Route path=":movieId" element={<Form />} /> 
+            </Route>
+          </Routes>
+      </BrowserRouter>
     );
 }
 
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Form: React.FC = () => {
+  return (
+      <h1>testando rota form</h1>
+  )
+}
+
+export default Form;
