17b9fd25ddbc2f3a4b8e0895092db02d10affd4f
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2022-05-10 15:51:28 -0300

    nav buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import MovieScore from '../MovieScore'
 
 import './styles.css'
@@ -23,7 +24,9 @@ const MovieCard: React.FC = () => {
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+        </Link>
       </div>
     </div>
   )
